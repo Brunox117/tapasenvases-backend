6eb37a9b26e685a3f19616c1c97bc1055d20d2b4
Bruno Jimenez <omarjm0910@gmail.com>
2025-11-25 22:51:15 -0600

    primera version de la creacion de usuarios

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
+  private readonly logger = new Logger('AuthService');
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+      await this.userRepository.save(user);
+      const { password: _, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    } catch (error) {
+      this.handleErrors(error);
+    }
   }
 
   findAll() {
@@ -22,5 +48,20 @@ export class AuthService {
 
   remove(id: string) {
     return `This action removes a #${id} auth`;
+  }
+
+  private handleErrors(error: any): never {
+    this.logger.error(error);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (error.code === '23505') {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      throw new BadRequestException(error.detail);
+    } else if (error instanceof HttpException) {
+      throw error;
+    } else {
+      throw new InternalServerErrorException(
+        'Unknown error please, contact the server admin',
+      );
+    }
   }
 }
