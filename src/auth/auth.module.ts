3f7e566ffdbcb1ca244be810eb5cc37e1705bc2d
Bruno Jimenez <omarjm0910@gmail.com>
2025-11-29 10:23:46 -0600

    configuracion inicial del JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
 })
 export class AuthModule {}
