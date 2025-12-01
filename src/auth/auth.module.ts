8c62ff57f1c58a471924b562123557936154e3d9
Bruno Jimenez <omarjm0910@gmail.com>
2025-11-30 19:48:59 -0600

    ajustamos la configuracion de jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
@@ -11,6 +13,14 @@ import { PassportModule } from '@nestjs/passport';
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+      }),
+    }),
   ],
 })
 export class AuthModule {}
